ff33a17b8ff06e4b227db04b725f9826ae099ec7
AntonioBDev <basi.dev9@gmail.com>
2026-05-18 22:52:54 -0700

    localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,50 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     eventoListeners();
-
+    darkModeListener();
     darkMode();
 })
 
-function eventoListeners(){
+function eventoListeners() {
     const mobilMenu = document.querySelector('.movil-menu');
 
     mobilMenu.addEventListener('click', navegacionResponsive);
 }
 
-function navegacionResponsive(e){
+function navegacionResponsive(e) {
     const navegacion = document.querySelector('.navegacion');
 
-    if(navegacion.classList.contains('mostrar')){
+    if (navegacion.classList.contains('mostrar')) {
         navegacion.classList.remove('mostrar');
-    }else{
-      navegacion.classList.add('mostrar');  
+    } else {
+        navegacion.classList.add('mostrar');
     }
 }
 
 //DarkMode 
-function darkMode(){
+function darkModeListener(){
+    const estadoDarkMode = localStorage.getItem('estadoDarkMode') ?? 'false';
+
+    if(estadoDarkMode == 'true'){
+        document.body.classList.add('dark-mode-body');
+    }
+}
+
+function darkMode() {
     const btn_darkMode = document.querySelector('.dark-mode');
 
-    btn_darkMode.addEventListener('click',function(){
+    btn_darkMode.addEventListener('click', function () {
         document.body.classList.toggle('dark-mode-body');
+
+        if(document.body.classList.contains('dark-mode-body')){
+            const estadoDarkmode = document.body.classList.contains('dark-mode-body');
+
+            localStorage.setItem('estadoDarkMode', estadoDarkmode);
+
+            
+        }else{
+            localStorage.removeItem('estadoDarkMode');
+        }
     });
+
 }
+
