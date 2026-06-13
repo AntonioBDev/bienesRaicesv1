722004ee7424f7abeb488a400909b84d889c3e78
AntonioBDev <basi.dev9@gmail.com>
2026-06-13 00:19:07 -0600

    backend file config

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -22,6 +22,7 @@ $vendedores_id = '';
 $errores = [];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
     $titulo = $_POST['titulo'] ?? '';
     $precio = $_POST['precio'] ?? '';
     $descripcion = $_POST['descripcion'] ?? '';
@@ -30,11 +31,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $estacionamientos = $_POST['estacionamientos'] ?? '';
     $vendedores_id = $_POST['vendedores_id'] ?? '';
     $creado = date("Y/m/d");
-    
-    echo '<pre>';
-    var_dump($_POST);
-    echo '</pre>';
-    exit;
+    //Guardar imagen
+    $imagen = $_FILES['imagen'];
+
 
     //Validar campos 
     if (!$titulo) {
@@ -64,14 +63,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $errores['vendedores_id'] = 'Elige un vendedor';
     }
 
+    //Validar imagen y tamaño 1mb = 1000byts
+    $medida = 1000 * 1000;
+    if (!$imagen['name'] || $imagen['error']) {
+        $errores['imagen'] = 'La imagen es obligatorio';
+    }
+    
+    if($imagen['size'] > $medida) {
+        $errores['imagen'] = 'La imagen supera los 100kb';
+    }
+
     //Validar que este vacio el arreglo de errores 
     if (empty($errores)) {
+        /*CREAR CARPETA */
+        $carpetaImagenes = '../../imagenes';
+        //Verificar que exista
+        if(!is_dir($carpetaImagenes)){
+            mkdir($carpetaImagenes);
+        }
+
+        // Crear nombre unico
+        $nombreArchivo = 
+
+        //subir imagen
+        move_uploaded_file($imagen['tmp_name'], $carpetaImagenes .'/'.'.jpg');
+
+        exit;
+
         //Insertar en la base de datos 
         $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamientos,creado, vendedores_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamientos','$creado', '$vendedores_id');";
 
         $resultado = mysqli_query($db, $query);
 
-        if($resultado){
+        if ($resultado) {
             header('Location: /admin?resultado=1');
         }
     }
@@ -83,12 +107,12 @@ incluirTemplate('header');
     <h2>Crear</h2>
     <a href="/admin" class="btn-verde">Regresar</a>
 
-    <form action="/admin/propiedades/crear.php" class="formulario" method="POST"  enctype="multipart/form-data">
+    <form action="/admin/propiedades/crear.php" class="formulario" method="POST" enctype="multipart/form-data">
         <fieldset>
             <legend>información General</legend>
             <div class="campo">
                 <label for="titulo">Titulo</label>
-                <input type="text" placeholder="Titulo de la propiedad" id="titulo" name="titulo" value="<?php echo $titulo?>">
+                <input type="text" placeholder="Titulo de la propiedad" id="titulo" name="titulo" value="<?php echo $titulo ?>">
                 <?php if (!empty($errores['titulo'])): ?>
                     <div class="alerta error"><?php echo $errores['titulo'] ?></div>
                 <?php endif ?>
@@ -96,8 +120,8 @@ incluirTemplate('header');
 
             <div class="campo">
                 <label for="precio">Precio</label>
-                <input type="number" placeholder="Precio de la propiedad" id="precio" name="precio" value="<?php echo $precio?>">
-                 <?php if (!empty($errores['precio'])): ?>
+                <input type="number" placeholder="Precio de la propiedad" id="precio" name="precio" value="<?php echo $precio ?>">
+                <?php if (!empty($errores['precio'])): ?>
                     <div class="alerta error"><?php echo $errores['precio'] ?></div>
                 <?php endif ?>
             </div><!--Campo  -->
@@ -105,12 +129,15 @@ incluirTemplate('header');
             <div class="campo">
                 <label for="imagen">Imagen</label>
                 <input type="file" id="imagen" accept="image/jpeg, image/png" name="imagen">
+                <?php if (!empty($errores['imagen'])): ?>
+                    <div class="alerta error"><?php echo $errores['imagen'] ?></div>
+                <?php endif ?>
             </div><!--Campo  -->
 
             <div class="campo">
                 <label for="descripcion">Descripción</label>
-                <textarea id="descripcion" name="descripcion"><?php echo $descripcion?></textarea>
-                 <?php if (!empty($errores['descripcion'])): ?>
+                <textarea id="descripcion" name="descripcion"><?php echo $descripcion ?></textarea>
+                <?php if (!empty($errores['descripcion'])): ?>
                     <div class="alerta error"><?php echo $errores['descripcion'] ?></div>
                 <?php endif ?>
             </div><!--Campo  -->
@@ -120,24 +147,24 @@ incluirTemplate('header');
             <legend>informació Propiedad</legend>
             <div class="campo">
                 <label for="habitaciones">Habitaciones</label>
-                <input type="number" placeholder="Ej: 3" min="1" max="9" id="habitaciones" name="habitaciones" value="<?php echo $habitaciones?>">
-                 <?php if (!empty($errores['habitaciones'])): ?>
+                <input type="number" placeholder="Ej: 3" min="1" max="9" id="habitaciones" name="habitaciones" value="<?php echo $habitaciones ?>">
+                <?php if (!empty($errores['habitaciones'])): ?>
                     <div class="alerta error"><?php echo $errores['habitaciones'] ?></div>
                 <?php endif ?>
             </div><!--Campo  -->
 
             <div class="campo">
                 <label for="wc">Baños</label>
-                <input type="number" placeholder="Ej: 3" min="1" max="9" id="wc" name="wc" value="<?php echo $wc?>">
-                 <?php if (!empty($errores['wc'])): ?>
+                <input type="number" placeholder="Ej: 3" min="1" max="9" id="wc" name="wc" value="<?php echo $wc ?>">
+                <?php if (!empty($errores['wc'])): ?>
                     <div class="alerta error"><?php echo $errores['wc'] ?></div>
                 <?php endif ?>
             </div><!--Campo  -->
 
             <div class="campo">
                 <label for="estacionamientos">Estacionamiento</label>
-                <input type="number" placeholder="Ej: 3" min="1" max="9" id="estacionamientos" name="estacionamientos" value="<?php echo $estacionamientos?>">
-                 <?php if (!empty($errores['estacionamientos'])): ?>
+                <input type="number" placeholder="Ej: 3" min="1" max="9" id="estacionamientos" name="estacionamientos" value="<?php echo $estacionamientos ?>">
+                <?php if (!empty($errores['estacionamientos'])): ?>
                     <div class="alerta error"><?php echo $errores['estacionamientos'] ?></div>
                 <?php endif ?>
             </div><!--Campo  -->
@@ -148,11 +175,11 @@ incluirTemplate('header');
             <div class="campo">
                 <select name="vendedores_id">
                     <option value="" selected disabled>-- Seleccione --</option>
-                    <?php while($row = mysqli_fetch_assoc($result_tablaVendedores)):?>
-                        <option <?php echo $vendedores_id === $row['id'] ? 'selected' : '' ?> value="<?php echo $row['id']?>"><?php echo $row['nombre'].' ' . $row['apellido']?></option>
-                    <?php endwhile?>
+                    <?php while ($row = mysqli_fetch_assoc($result_tablaVendedores)): ?>
+                        <option <?php echo $vendedores_id === $row['id'] ? 'selected' : '' ?> value="<?php echo $row['id'] ?>"><?php echo $row['nombre'] . ' ' . $row['apellido'] ?></option>
+                    <?php endwhile ?>
                 </select>
-                 <?php if (!empty($errores['vendedores_id'])): ?>
+                <?php if (!empty($errores['vendedores_id'])): ?>
                     <div class="alerta error"><?php echo $errores['vendedores_id'] ?></div>
                 <?php endif ?>
             </div><!--Campo  -->
--- include/funciones.php
+++ include/funciones.php
@@ -1,6 +1,15 @@
-<?php 
+<?php
 require 'app.php';
 
-function incluirTemplate(string $nombre, bool $inicio = false){
+function incluirTemplate(string $nombre, bool $inicio = false)
+{
     include TEMPLATES_URL . "/{$nombre}.php";
+}
+
+function debug($dato)
+{
+    echo '<pre>';
+    var_dump($dato);
+    echo '</pre>';
+    exit;
 }
