8df5a96dca0562a74d1eb21727627405023d4015
AntonioBDev <basi.dev9@gmail.com>
2026-07-04 23:00:00 -0600

    scss implementacion

--- include/config/database.php
+++ include/config/database.php
@@ -7,11 +7,19 @@ function conectarDB() : mysqli{
         // $nombreProyecto= 'bienesraices_crud_2';
         
         //remoto
-        $host = '192.168.1.88';
-        $perfil = 'remote';
-        $password = 'rootroot123*';
-        $nombreProyecto = 'bienesraices_crud_2';
+        // $host = '192.168.1.88';
+        // $perfil = 'remote';
+        // $password = 'rootroot123*';
+        // $nombreProyecto = 'bienesraices_crud_2';
+        // $db = mysqli_connect($host, $perfil, $password, $nombreProyecto);
+
+        //remoto hostinger
+        $host = 'srv526.hstgr.io';
+        $perfil = 'u842510402_bienesraices';
+        $password = 'RneQ9RO1';
+        $nombreProyecto = 'u842510402_b_r_crud2';
         $db = mysqli_connect($host, $perfil, $password, $nombreProyecto);
+
 
         if(!$db){
             echo 'No se pudo conectar';
